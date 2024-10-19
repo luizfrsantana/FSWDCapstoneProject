7c2487056cfde240a04eb2e18fb179cba5fcba70
Luiz Felipe Santana <luizfrsantana@gmail.com>
2024-10-19 16:17:05 +0000

    adding img

--- FrontEnd/frontend/src/App.js
+++ FrontEnd/frontend/src/App.js
@@ -1,20 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from './components/Navbar'
-import Users from './components/Users'
-import UserCreate from './components/UserCreate'
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' component={Users} />
-          <Route exact path='/create' component={UserCreate} />
-        </Routes>
-      </div>
-    </Router>
+    <h1> Ola</h1>
   );
 }
